fceccc2091d61c9f387b704263efc9b805899d6e
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-03-23 13:12:48 +0200

    Implement restaurant list display with modal interaction

--- event-assignments/t2/t2.js
+++ event-assignments/t2/t2.js
@@ -771,3 +771,45 @@ const restaurants = [
 ];
 
 // your code here
+const taulukko = document.querySelector('#target');
+const modal = document.querySelector('#modal');
+
+// restaurants aakkosjärjestykseen
+restaurants.sort(function (a, b) {
+  return a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1;
+});
+
+for (const restaurant of restaurants) {
+  // rivi
+  const tr = document.createElement('tr');
+  tr.addEventListener('click', function () {
+    for (const elem of document.querySelectorAll('.highlight')) {
+      elem.classList.remove('highlight');
+    }
+
+    tr.classList.add('highlight');
+
+    // tyhjennä modal
+    modal.innerHTML = '';
+    // avaa modal
+    modal.showModal();
+    // tee modalin sisältö
+    const nameH3 = document.createElement('h3');
+    nameH3.innerText = restaurant.name;
+
+    modal.append(nameH3);
+  });
+
+  // nimisolu
+  const nameTd = document.createElement('td');
+  nameTd.innerText = restaurant.name;
+  // osoitesolu
+  const addressTd = document.createElement('td');
+  addressTd.innerText = restaurant.address;
+  // kaupunkisolu
+  const cityTd = document.createElement('td');
+  cityTd.innerText = restaurant.city;
+  // lisätään solut riviin
+  tr.append(nameTd, addressTd, cityTd);
+  taulukko.append(tr);
+}
