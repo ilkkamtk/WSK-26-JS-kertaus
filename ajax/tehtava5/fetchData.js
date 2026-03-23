853f1dd96707cac94553e8b85b782bff2bd59b97
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-03-23 13:29:05 +0200

    Add initial implementation for restaurant data fetching and display

---
+++ ajax/tehtava5/fetchData.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+const fetchData = async (url, options) => {
+  try {
+    const data = await fetch(url, options);
+
+    if (data.status < 200 || data.status >= 300) {
+      throw new Error('Oma virheviesti tähän');
+    }
+
+    return await data.json();
+  } catch (error) {
+    console.log('ei toimi', error);
+  }
+};
